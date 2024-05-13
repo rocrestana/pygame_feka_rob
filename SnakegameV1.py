104eaacfa225aafe58fc342239ed1c29bfd43dba
rocrestana <rocrestana@gmail.com>
2024-05-13 16:40:32 -0300

    img e som

--- SnakegameV1.py
+++ SnakegameV1.py
@@ -12,3 +12,19 @@ tmnh_bloco_map = 40
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Snake Game')
 tempo = pygame.time.Clock()
+
+keys_down = {}
+assets = {} #dicionario de imagens e sons
+assets['fundo'] =  pygame.image.load('campo.png').convert()
+assets['fundo'] = pygame.transform.scale(assets['fundo'], (WIDTH,HEIGHT))
+assets['cobra'] = pygame.image.load('OIG1.jpg').convert()
+assets['cobra']= pygame.transform.scale(assets['cobra'], (40,40))
+assets['apple'] = pygame.image.load('lixo.png').convert()
+assets['apple']= pygame.transform.scale(assets['apple'],(tmnh_bloco_map,tmnh_bloco_map))
+assets['maracana'] = pygame.image.load('maracana.jpg').convert()
+assets['maracana'] = pygame.transform.scale(assets['maracana'],(WIDTH,HEIGHT))
+assets['FLA'] = pygame.transform.scale(assets['apple'],(tmnh_bloco_map*4, tmnh_bloco_map*4))
+assets['FLU'] = pygame.image.load('Fluminense_FC_escudo.png').convert()
+assets['FLU'] = pygame.transform.scale(assets['FLU'],(tmnh_bloco_map*4, tmnh_bloco_map*4))
+assets['som'] = pygame.mixer.music.load('assets/snd/tgfcoder-FrozenJam-SeamlessLoop.ogg')
+assets['som'] = pygame.mixer.music.set_volume(0.4)
