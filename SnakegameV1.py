9407c9e79ffc704415cbde916fa7fa85e306af73
rocrestana <rocrestana@gmail.com>
2024-05-20 16:21:41 -0300

    cria mapa fora

--- SnakegameV1.py
+++ SnakegameV1.py
@@ -4,23 +4,14 @@ import os
 from classes import *
 from assets import *
 from dimensoes import *
+from criacao_de_mapa import *
+# pip install -r requirements.txt
+
 
 pygame.init()
 pygame.mixer.init()
 
 keys_down = {}
-
-def Cria_Mapa():
-    for x in range(0, WIDTH, tmnh_bloco_map):
-        for y in range(0, HEIGHT, tmnh_bloco_map):
-            rect = pygame.Rect(x, y, tmnh_bloco_map, tmnh_bloco_map)
-            pygame.draw.rect(window,(255,255,255,0.5), rect, 1)
-
-score = font.render('1',True,"white")
-score_rect = score.get_rect(center =(WIDTH/2, HEIGHT/20))
-velocidade = font4.render('1',True,"Black")
-velo_rect = velocidade.get_rect(center = (1,HEIGHT/20))
-Cria_Mapa()
 
 snake = Snake()
 apple = Apple()  # Movido para fora do loop principal
